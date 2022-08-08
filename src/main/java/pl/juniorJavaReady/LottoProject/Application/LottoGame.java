f450931424bee0e6db3bc8a3eb6d2a1f013cd679
karolinakaczmarczyk <kaczmarczyk.kar@gmail.com>
2022-08-09 01:27:52 +0200

    more of Komac's tips implemented, still to do are messages to properties and lottogame class (constructor dependancy injection?)

--- src/main/java/pl/juniorJavaReady/LottoProject/Application/LottoGame.java
+++ src/main/java/pl/juniorJavaReady/LottoProject/Application/LottoGame.java
@@ -9,21 +9,21 @@ import java.util.Set;
 public class LottoGame implements Game {
 
     // TODO: 30.07.2022 should be an interface here (as in previous comment), also programming to interfaces
-    //like Game interface?
-    private final ConsoleInputReader userInputGetter;
+    //like Game interface..?
+    private final ConsoleInputReader userInputReader;
     private final LottoNumbersGenerator randomNumbersGenerator;
     private final LottoOutcomeVerifier gameOutcomeVerifier;
 
     // TODO: 30.07.2022 dependency injection, currently class is not testable
-    public LottoGame() {
-        this.userInputGetter = new ConsoleInputReader();
-        this.randomNumbersGenerator = new LottoNumbersGenerator();
-        this.gameOutcomeVerifier = new LottoOutcomeVerifier();
+    public LottoGame(ConsoleInputReader reader, LottoNumbersGenerator generator, LottoOutcomeVerifier verifier) {
+        this.userInputReader = reader;
+        this.randomNumbersGenerator = generator;
+        this.gameOutcomeVerifier = verifier;
     }
 
     @Override
     public void play() {
-        Set<Integer> userGuesses = userInputGetter.acquireUserInput();
+        Set<Integer> userGuesses = userInputReader.acquireUserInput();
         Set<Integer> randomNumbers = randomNumbersGenerator.generateRandomNumbers();
         gameOutcomeVerifier.checkGameOutcome(randomNumbers, userGuesses);
     }
--- src/main/java/pl/juniorJavaReady/LottoProject/GenerateRandomNumbers/LottoNumbersGenerator.java
+++ src/main/java/pl/juniorJavaReady/LottoProject/GenerateRandomNumbers/LottoNumbersGenerator.java
@@ -7,34 +7,35 @@ import java.util.Random;
 import java.util.Set;
 
 public class LottoNumbersGenerator implements RandomNumbersGenerator {
-    // TODO: 30.07.2022 should be injected in constructor, this way its not testable
-    private final Random random = new Random();
-    // TODO: 30.07.2022 should be a part of a method, if it is here object is not reusable
-    private final Set<Integer> generatedNumbers;
+    private final Random random;
 
-    public LottoNumbersGenerator() {
-        this.generatedNumbers = new HashSet<>();
+    public LottoNumbersGenerator(Random random) {
+        this.random = random;
     }
 
     // TODO: 30.07.2022 without checking the implementation idk how many numbers will be generated and in what range. its also not extendable: what if i wanted to generate 3 numbers? or 20? what if i wanted a different range of numbers?
+    // Answer: added an interface, so method's implementation could differ in another type of game;
+    // as to extention possibilities, why is changing value of numbers_for_lotto_game a bad thing?
+    // i'm guessing there's s'where a better solution? s'thing like generics?
     @Override
     public Set<Integer> generateRandomNumbers() {
+        final Set<Integer> generatedNumbers = new HashSet<>();
         while (generatedNumbers.size() < LottoGameSettings.NUMBERS_FOR_LOTTO_GAME) {
             generatedNumbers.add(getRandomNumber());
         }
-        printGeneratedNumbers();
+        printGeneratedNumbers(generatedNumbers);
         return generatedNumbers;
     }
 
     private int getRandomNumber() {
-        // TODO: 30.07.2022 this will generate you numbers from 1 to 98, but based on messages i guess it should be up to 99
-        return random.nextInt(LottoGameSettings.LOWER_BOUND, LottoGameSettings.UPPER_BOUND);
+        return random.nextInt(LottoGameSettings.LOWER_BOUND, LottoGameSettings.UPPER_BOUND + 1);
     }
 
-    private void printGeneratedNumbers() {
+    private void printGeneratedNumbers(Set<Integer> numbers) {
         // TODO: 30.07.2022 Generated numbers are:
+        // Answer: I like the suspense ;)
         System.out.println("\nGenerating numbers.\n" +
-                "\nNumbers generated are:\n" +
-                generatedNumbers);
+                "\nGenerated numbers are:\n" +
+                numbers);
     }
 }
